ce9b7f903c0fc6682105b67080f53968273a9b8c
JordyPouw <jordypouw@gmail.com>
2017-09-08 18:23:22 +0200

    update gitignore and watchfiles

--- gulp_tasks/task.watch.js
+++ gulp_tasks/task.watch.js
@@ -10,8 +10,8 @@ module.exports = function (gulp) {
     gulp.watch('index.html').on('change', config.browsersync.reload);
     gulp.watch('src/css/**/*.scss', ['css']);
     gulp.watch('src/js/**/*.js', ['js']);
-    gulp.watch('src/fonts/**/*.{eot,woff2,woff,ttf,svg}', ['font']);
-    gulp.watch('src/sounds/**/*.{mp3,ogg}', ['sound']);
+    gulp.watch('src/font/**/*.{eot,woff2,woff,ttf,svg}', ['font']);
+    gulp.watch('src/sound/**/*.{mp3,ogg}', ['sound']);
     gulp.watch('src/img/**/*.{jpg,png,svg}', ['img']);
 
   });
